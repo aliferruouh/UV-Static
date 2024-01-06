ad48747bcd00e04eb5b6e337afdd9766046381ea
aliferruouh <142675770+aliferruouh@users.noreply.github.com>
2024-01-06 15:46:37 +0100

    Update error.js

--- static/error.js
+++ static/error.js
@@ -1,6 +1,20 @@
-function showAlert() {
-    alert('Hello, this is a simple alert!');
+"use strict";
+const error = document.getElementById("uv-error");
+const errorCode = document.getElementById("uv-error-code");
+const registerButton = document.getElementById("uv-register-sw");
+
+if (location.pathname.startsWith(__uv$config.prefix)) {
+  error.textContent = "Error: The service worker is not registered.";
+  registerButton.classList.add("show");
 }
 
-// You can call the function when needed
-showAlert();
+registerButton.addEventListener("click", async () => {
+  try {
+    await registerSW();
+    location.reload();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    registerButton.classList.remove("show");
+  }
+}); 
